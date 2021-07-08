c5494633a10fd923d64a2c4cba76f2738edccfde
hoycid <quilangcidrex@gmail.com>
2021-07-08 22:21:15 +0800

    formatted code

--- pages/api/inbound/index.js
+++ pages/api/inbound/index.js
@@ -6,26 +6,26 @@ dbConnect();
 export default async (req, res) => {
   const { method } = req;
 
-  switch(method) {
-    case 'GET':
+  switch (method) {
+    case "GET":
       try {
-        const inbound = await Inbound.find({}).sort({date: -1});
-        res.status(200).json({ success: true, data: inbound })
-      } catch(err) {
-        res.status(400).json({ success: false, err});
+        const inbound = await Inbound.find({}).sort({ date: -1 });
+        res.status(200).json({ success: true, data: inbound });
+      } catch (err) {
+        res.status(400).json({ success: false, err });
       }
       break;
-    case 'POST': 
+    case "POST":
       try {
         const inbound = await Inbound.create(req.body);
-        res.status(201).json({ success: true, data: inbound })
-      } catch(err) {
+        res.status(201).json({ success: true, data: inbound });
+      } catch (err) {
         const data = req.body;
-        res.status(400).json({ success: false, error: err});
+        res.status(400).json({ success: false, error: err });
       }
       break;
     default:
-      res.status(400).json({ success: false, err});
+      res.status(400).json({ success: false, err });
       break;
   }
-}
+};
